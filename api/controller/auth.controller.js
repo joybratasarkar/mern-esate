329cea04975b01c28d75a214b7123dd517c06578
joybratasarkar <joybrata007@gmail.com>
2024-03-31 03:51:51 +0530

    signup api created

---
+++ api/controller/auth.controller.js
@@ -0,0 +1,17 @@
+import User from '../model/user.model.js'
+import bcryptjs from 'bcryptjs'
+
+export const signup = async (req, res) => {
+    const { username, email, password } = req.body
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+    const newUser = new User({ username: username, email: email, password: hashedPassword })
+   try{
+    await newUser.save()
+    res.status(201).json('User Create Succesfully')
+   }
+   catch(error){
+    res.status(500).json(error.message)
+
+   }
+   
+};
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 
 dotenv.config();
@@ -14,9 +15,11 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 
 const app = express();
 const PORT = 3000;
-
+app.use(express.json());
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}!`);
 });
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
