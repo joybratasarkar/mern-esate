dc40ae0add56a4544d37f4f33703a47f9d08ab7f
joybratasarkar <joybrata007@gmail.com>
2024-03-31 04:16:12 +0530

    error hander created

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../model/user.model.js'
 import bcryptjs from 'bcryptjs'
-
-export const signup = async (req, res) => {
+import {errorHandler} from '../utils/error.js'
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body
     const hashedPassword = bcryptjs.hashSync(password, 10)
     const newUser = new User({ username: username, email: email, password: hashedPassword })
@@ -10,6 +10,8 @@ export const signup = async (req, res) => {
     res.status(201).json('User Create Succesfully')
    }
    catch(error){
+
+    // next(errorHandler(550,'error from the function'))
     res.status(500).json(error.message)
 
    }
